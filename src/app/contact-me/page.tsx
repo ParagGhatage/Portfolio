16abab61d23c6b0a0b5c9a8bb90b85e8efbda27c
ParagGhatage <phghatage1@gmail.com>
2024-06-19 19:48:10 +0530

    Portfolio first commit

---
+++ src/app/contact-me/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Page() {
+  return (
+    <div className="text-5xl text-center mt-40">
+        Name: ParagGhatage
+        <div className="mt-6">
+        Email: phghatage1@gmail.com
+        </div>
+        
+    </div>
+  );
+}
