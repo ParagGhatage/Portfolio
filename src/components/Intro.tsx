d99077ee430f8b149b6d12146fe0c8b6b0800f87
ParagGhatage <phghatage1@gmail.com>
2024-06-19 19:57:58 +0530

    apostrophy

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -14,7 +14,7 @@ export function Intro() {
         className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
       >
         <h2 className="text-white text-3xl md:text-6xl font-bold text-center  ">
-        Hi, I'm Parag
+        Hi, I&apos;m Parag
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center text">
           
