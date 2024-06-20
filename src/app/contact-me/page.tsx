75bb324195d12ea1b594e272be7098e6db9e79a5
ParagGhatage <phghatage1@gmail.com>
2024-06-20 16:44:18 +0530

    Contact Me page

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
+import { ContactMe } from "@/components/ContactMe";
+
 export default function Page() {
   return (
-    <div className="text-5xl text-center mt-40">
-        Name: ParagGhatage
-        <div className="mt-6">
-        Email: phghatage1@gmail.com
-        </div>
-        
+    <div className="mt-20">
+          <ContactMe/>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -12,6 +15,9 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
       animation: {
         shimmer: "shimmer 2s linear infinite",
@@ -28,6 +34,18 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
