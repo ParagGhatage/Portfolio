04f62140569b1b7aeb715ce8cc10e2b105d4400d
ParagGhatage <phghatage1@gmail.com>
2024-06-22 13:28:49 +0530

    chakra ui

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -12,19 +12,18 @@ import {
   IconBrandLinkedin
 } from "@tabler/icons-react";
 import axios from "axios"
-import toast from "react-hot-toast";
+import { useToast } from '@chakra-ui/react'
 
 export function ContactMe() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
+  const toast = useToast()
 
   const [user, setUser] = React.useState({
     email: "",
     message: "",
     name: "",
 })
+ 
+
     const onEmail = async () => {
       try {
 
@@ -32,9 +31,18 @@ export function ContactMe() {
           console.log( response.data);
 
           
+          if(response){
+            toast({
+              title: 'Email Sent.',
+              description: "We have sent an email to Parag.",
+              status: 'success',
+              duration: 9000,
+              isClosable: true,
+            })
+          }
           
       } catch (error:any) {
-          console.log("Signup failed", error.message);
+          console.log("Enable to send email", error.message);
           
       }
     }
@@ -48,7 +56,7 @@ export function ContactMe() {
         Send me an email right from here
       </p>
 
-      <form className="my-8" onSubmit={handleSubmit}>
+      <form className="my-8" onSubmit={onEmail}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="name">Your Name</Label>
@@ -76,7 +84,7 @@ export function ContactMe() {
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
-          onClick={onEmail}
+          
         >
           Send Email &rarr;
           <BottomGradient />
