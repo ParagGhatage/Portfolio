7114ab5c18b64a4f335ddc680bebc24b4a9c1c63
ParagGhatage <phghatage1@gmail.com>
2024-06-23 13:39:18 +0530

    Connectify add

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -30,6 +30,31 @@ export function AnimatedPin() {
         />
         </div>
       </PinContainer>
+
+      <PinContainer
+        title="https://thunder-tube.vercel.app/"
+        href="https://thunder-tube.vercel.app/"
+      >
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+            Connectify
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 ">
+              Brings all social media apps at one place
+            </span>
+          </div>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <Image
+          src={`https://images.pexels.com/photos/5011647/pexels-photo-5011647.jpeg?auto=compress&cs=tinysrgb&w=600`}
+          alt="Connectify"
+          height="1000"
+          width="400"
+          className="object-contain"
+        />
+        </div>
+      </PinContainer>
+
     </div>
   );
 }
--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -72,6 +72,37 @@ export function CardGradient() {
         
       </BackgroundGradient>
       </a>
+
+      <a href="https://thunder-tube.vercel.app/">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        
+      <Image
+          src={`https://images.pexels.com/photos/5011647/pexels-photo-5011647.jpeg?auto=compress&cs=tinysrgb&w=600`}
+          alt="Connectify"
+          height="700"
+          width="400"
+          className="object-contain"
+        />
+    
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          Connectify
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          Messaging web app that
+
+          brings all your social media apps at one place.
+
+        </p>
+        <div className=" p-5 w-20 rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+        
+        <a href="https://github.com/ParagGhatage/Connectify">Source Code</a>
+        
+        </div>
+        
+        
+      </BackgroundGradient>
+      </a>
       
     </div>
   );
