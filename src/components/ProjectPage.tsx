3d8a1bc29f02d8b364fdd084f833b01689599eb7
ParagGhatage <phghatage1@gmail.com>
2024-07-19 09:05:27 +0530

    structural changes

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -8,85 +8,99 @@ import Link from "next/link";
 
 export function Sparkles() {
   return (
-    <div className="h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-  <h1 className="text-2xl md:text-3xl lg:text-7xl font-bold text-center text-white relative z-20">
-    Projects
-  </h1>
-  <div className="w-full md:w-[40rem] h-40 relative">
-    {/* Gradients */}
-    <div className="absolute inset-x-5 sm:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-    <div className="absolute inset-x-5 sm:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-    <div className="absolute inset-x-10 sm:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-    <div className="absolute inset-x-10 sm:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+    <div className="h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md mt-20">
+      <h1 className="text-2xl md:text-3xl lg:text-7xl font-bold text-center text-white relative z-20">
+        Projects
+      </h1>
+      <div className="w-full md:w-[40rem] h-40 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-5 sm:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-5 sm:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-10 sm:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-10 sm:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-    {/* Core component */}
-    <SparklesCore
-      background="transparent"
-      minSize={0.4}
-      maxSize={2}
-      speed={1}
-      particleDensity={1200}
-      className="w-full h-full"
-      particleColor="E633FF"
-    />
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={2}
+          speed={1}
+          particleDensity={1200}
+          className="w-full "
+          particleColor="E633FF"
+        />
 
-    {/* Radial Gradient to prevent sharp edges */}
-    <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-  </div>
-</div>
-
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full  bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
+    </div>
   );
 }
 
-
 export function CardGradient() {
   return (
-    <div className="flex flex-col sm:flex-row justify-around items-center sm:items-start">
-  <Link href="https://thunder-tube.vercel.app/" className="mb-4 sm:mb-0">
-    <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-      <Image
-        src="https://images.pexels.com/photos/2823936/pexels-photo-2823936.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt="Thunder-Tube"
-        height="700"
-        width="400"
-        className=""
-      />
-      <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-        Thunder-Tube
-      </p>
-      <p className="text-sm text-neutral-600 dark:text-neutral-400">
-        Full stack YouTube clone with all its features.
-        It uses ReactJS as a frontend framework,
-        MongoDB as a database and
-        Express and NodeJS for backend.
-      </p>
-      <div className="p-2 sm:p-5 w-32 rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-        <Link href="https://github.com/ParagGhatage/Thunder-Tube">Source Code</Link>
-      </div>
-    </BackgroundGradient>
-  </Link>
+    <div className="flex flex-col  sm:flex-row justify-around items-center sm:items-start ">
+      <Link href="https://thunder-tube.vercel.app/" className="sm:mb-0">
+        <BackgroundGradient className="rounded-[22px] max-w-sm w-full sm:w-auto flex flex-col justify-between p-4 sm:p-10 bg-white dark:bg-zinc-900">
+          <div className="p-4">
+            click to explore &rarr;
+          </div>
+          <div className="flex flex-col ">
+            <Image
+              src="https://images.pexels.com/photos/2823936/pexels-photo-2823936.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt="Thunder-Tube"
+              height="700"
+              width="400"
+              className=""
+            />
+            <div className="sm:mb-20">
+              <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                Thunder-Tube
+              </p>
+              <p className="text-sm text-neutral-600  dark:text-neutral-400 ">
+                Full stack YouTube clone with all its features. It uses ReactJS as a
+                frontend framework, MongoDB as a database, and Express and NodeJS for
+                backend.
+              </p>
+            </div>
+          </div>
+          <div className="p-5 sm:p-5 w-38 rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xl font-bold dark:bg-zinc-800">
+            <Link href="https://github.com/ParagGhatage/Thunder-Tube">Source Code</Link>
+          </div>
+        </BackgroundGradient>
+      </Link>
 
-  <Link href="https://voyagevista-beige.vercel.app/" className="mb-4 sm:mb-0">
-    <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-      <Image
-        src="https://images.pexels.com/photos/2178175/pexels-photo-2178175.jpeg?auto=compress&cs=tinysrgb&w=600"
-        alt="Connectify"
-        height="700"
-        width="400"
-        className="object-contain"
-      />
-      <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-        VoyageVista
-      </p>
-      <p className="text-sm text-neutral-600 dark:text-neutral-400">
-      VoyageVista is an innovative web application designed to help users discover and plan their perfect trips. Whether you&apos;re an avid traveler or someone looking to explore new destinations, our platform offers comprehensive tools and features to enhance your travel planning experience.
-      </p>
-      <div className="p-2 sm:p-5 w-32 rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-        <Link href="https://github.com/ParagGhatage/VoyageVista">Source Code</Link>
-      </div>
-    </BackgroundGradient>
-  </Link>
-</div>
-
+      <Link href="https://voyagevista-beige.vercel.app/" className="sm:mb-0">
+        <BackgroundGradient className="rounded-[22px] max-w-sm w-full sm:w-auto flex flex-col  p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <div className="p-4">
+            click to explore &rarr;
+          </div>
+          <div className="flex flex-col  ">
+            <Image
+              src="https://images.pexels.com/photos/2178175/pexels-photo-2178175.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="Explora"
+              height="700"
+              width="300"
+              className=""
+            />
+            <div>
+              <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                Explora
+              </p>
+              <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                Explora is an innovative web application designed to help users discover
+                and plan their perfect trips. Whether you&apos;re an avid traveler or
+                someone looking to explore new destinations, our platform offers
+                comprehensive tools and features to enhance your travel planning
+                experience.
+              </p>
+            </div>
+          </div>
+          <div className="p-5 sm:p-5 w-38 rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xl font-bold dark:bg-zinc-800">
+            <Link href="https://github.com/ParagGhatage/VoyageVista">Source Code</Link>
+          </div>
+        </BackgroundGradient>
+      </Link>
+    </div>
   );
 }
