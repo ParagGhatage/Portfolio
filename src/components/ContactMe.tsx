c3c5047361fac94af611954cfc6e41c8d38b4556
ParagGhatage <phghatage1@gmail.com>
2024-07-21 23:40:47 +0530

    mailto link

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -99,7 +99,17 @@ export function ContactMe() {
         <div className="text-center m-4 text-2xl ">
             Socials
         </div>
-        
+        <Link href="mailto:thunderwolf.dev@gmail.com?subject=Hello%20there">
+          <div
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+          > 
+            <IconBrandGmail className="h-4  text-neutral-800 dark:text-neutral-300" /> 
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm text-center">
+              Gmail
+            </span>
+            <BottomGradient />
+          </div>
+          </Link>
 
         <Link href="https://github.com/ParagGhatage">
           <div
